c78b916ee63fcf57bc552eac45fa2e88ff6df5fa
Mashook-Mohammad-Meshkat <meshkatmashook00@gmail.com>
2024-10-10 18:28:27 +0600

    mycv

---
+++ src/App.vue
@@ -0,0 +1,327 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <!-- <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header> -->
+
+  <main>
+    <body class="w3-light-grey">
+      <!-- Page Container -->
+      <div class="w3-content w3-margin-top" style="max-width: 1400px">
+        <!-- The Grid -->
+        <div class="w3-row-padding">
+          <!-- Left Column -->
+          <div class="w3-third w3-animate-left">
+            <div class="w3-white w3-text-grey w3-card-4">
+              <div class="w3-display-container">
+                <img
+                  src="https://media.licdn.com/dms/image/D5603AQHukIh1d1mrBw/profile-displayphoto-shrink_200_200/0/1665563396473?e=2147483647&v=beta&t=mEfywgb_D1UIyHcLQxKq1DqwYGYOQq0-_r9JLfqO3W8"
+                  style="width: 100%"
+                  alt="Avatar"
+                />
+                <div class="w3-display-bottomleft w3-container w3-text-white">
+                  <h2>Mashook Mohammad Meshkat</h2>
+                </div>
+              </div>
+              <div class="w3-container">
+                <p>
+                  <i class="fa fa-briefcase fa-fw w3-margin-right w3-large w3-text-teal"></i
+                  >Engineer | Developer | Researcher
+                </p>
+                <p>
+                  <i class="fa fa-home fa-fw w3-margin-right w3-large w3-text-teal"></i>Dhaka,
+                  Bangladesh
+                </p>
+                <p>
+                  <a href="mailto:meshkatmashook00@gmail.com" class="w3-text-teal">
+                    <i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-teal"></i>
+                    meshkatmashook00@gmail.com
+                  </a>
+                </p>
+                <p>
+                  <i class="fa fa-phone fa-fw w3-margin-right w3-large w3-text-teal"></i
+                  >+8801775725487
+                </p>
+                <p>
+                  <i class="fa fa-linkedin w3-hover-opacity w3-text-teal"></i
+                  ><a
+                    class="w3-text-teal"
+                    href="https://www.linkedin.com/in/mashook-mohammad-meshkat"
+                    target="_blank"
+                    style="margin-left: 25px"
+                    >Linked In</a
+                  >
+                </p>
+                <hr />
+
+                <p class="w3-large">
+                  <b><i class="fa fa-asterisk fa-fw w3-margin-right w3-text-teal"></i>Skills</b>
+                </p>
+                <p>Python/C/C++ programming and OOP</p>
+                <div class="w3-light-grey w3-round-xlarge w3-small">
+                  <div class="w3-container w3-center w3-round-xlarge w3-blue" style="width: 90%">
+                    90%
+                  </div>
+                </div>
+                <p>MySQL | PyCharm | Visual Studio Code</p>
+                <div class="w3-light-grey w3-round-xlarge w3-small">
+                  <div class="w3-container w3-center w3-round-xlarge w3-green" style="width: 80%">
+                    <div class="w3-center w3-text-white">80%</div>
+                  </div>
+                </div>
+                <p>MATLAB | Simulink | Proteus | PSpice | HOMER | AutoCAD</p>
+                <div class="w3-light-grey w3-round-xlarge w3-small">
+                  <div class="w3-container w3-center w3-round-xlarge w3-orange" style="width: 75%">
+                    75%
+                  </div>
+                </div>
+                <p>MS Excel | MS Word | MS PowerPoint</p>
+                <div class="w3-light-grey w3-round-xlarge w3-small">
+                  <div class="w3-container w3-center w3-round-xlarge w3-red" style="width: 70%">
+                    70%
+                  </div>
+                </div>
+                <br />
+
+                <p class="w3-large w3-text-theme">
+                  <b><i class="fa fa-globe fa-fw w3-margin-right w3-text-teal"></i>Languages</b>
+                </p>
+                <p>English</p>
+                <div class="w3-light-grey w3-round-xlarge">
+                  <div class="w3-round-xlarge w3-center w3-blue" style="height: 24px; width: 90%">
+                    90%
+                  </div>
+                </div>
+                <p>Bangla</p>
+                <div class="w3-light-grey w3-round-xlarge">
+                  <div class="w3-round-xlarge w3-center w3-green" style="height: 24px; width: 80%">
+                    80%
+                  </div>
+                </div>
+                <p>Hindi and Urdu</p>
+                <div class="w3-light-grey w3-round-xlarge">
+                  <div class="w3-round-xlarge w3-center w3-orange" style="height: 24px; width: 55%">
+                    55%
+                  </div>
+                </div>
+                <p>Arabic</p>
+                <div class="w3-light-grey w3-round-xlarge">
+                  <div class="w3-round-xlarge w3-center w3-red" style="height: 24px; width: 20%">
+                    20%
+                  </div>
+                </div>
+                <br />
+              </div>
+            </div>
+            <br />
+
+            <!-- End Left Column -->
+          </div>
+
+          <!-- Right Column -->
+          <div class="w3-twothird">
+            <div
+              class="w3-container w3-card w3-white animate__animated animate__backInDown w3-margin-bottom"
+            >
+              <h2 class="w3-text-grey w3-padding-16">
+                <i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Work
+                Experience
+              </h2>
+              <div class="w3-container">
+                <h5 class="w3-opacity">
+                  <b
+                    >Managment Trainee Officer - Tech /
+                    <a
+                      href="https://drive.google.com/file/d/1wpP0chF3iATnjZRFCec8_S8x8tBD5mXi/view"
+                      target="_blank"
+                      >WeGro Technologies ltd.</a
+                    ></b
+                  >
+                </h5>
+                <h6 class="w3-text-teal">
+                  <i class="fa fa-calendar fa-fw w3-margin-right"></i>Aug 2024 -
+                  <span class="w3-tag w3-teal w3-round">Current</span>
+                </h6>
+                <p>Working as a trainee in the technology department of WeGro Technologies ltd.</p>
+                <p>
+                  Learning web development alongside working with website manual development and map
+                  insights
+                </p>
+                <hr />
+              </div>
+              <div class="w3-container">
+                <h5 class="w3-opacity">
+                  <b
+                    >Teacher's Assistant -
+                    <a href="https://www.bracu.ac.bd/" target="_blank">BRAC University</a></b
+                  >
+                </h5>
+                <h6 class="w3-text-teal">
+                  <i class="fa fa-calendar fa-fw w3-margin-right"></i>June 2022 - May 2024
+                </h6>
+                <p>
+                  ▪ Recognized as one of the Best TAs in the department and Senior TA coordinator
+                  to guide junior TAs
+                  <br />
+                  ▪ Supported 250+ undergraduate students with helpful study habits and exam
+                  strategies for 6 consecutive semesters as TA
+                  <br />
+                  ▪ Applied different teaching techniques through taking 90+ classes and 120+
+                  consultation sessions to improve student academic achievement, build test skills
+                  and foster classroom confidence.
+                  <br />
+                  ▪ Assisted respective faculty members by making 20+ study and course-related
+                  materials <br />
+                  ▪ Checked 500+ scripts while taking viva and presentations of the students
+                </p>
+                <hr />
+              </div>
+            </div>
+
+            <div class="w3-container w3-card w3-white animate__animated animate__backInDown">
+              <h2 class="w3-text-grey w3-padding-16">
+                <i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i
+                >Education
+              </h2>
+              <div class="w3-container">
+                <h5 class="w3-opacity"><b>BRAC University</b></h5>
+                <h6 class="w3-text-teal">
+                  <i class="fa fa-calendar fa-fw w3-margin-right"></i>January 2020 - May 2024
+                </h6>
+                <p>
+                  Bachelor of Science in Electrical and Electronic Engineering (BSEEE) <br />
+                  BRAC University | EEE Specialization: Power | CGPA: 3.97 | Placed in VC’s List 11
+                  times <br />
+                  Specialization CGPA: 4.00 | Waiver: 100%/75% Scholarship on Merit |
+                  <a
+                    href="https://drive.google.com/file/d/1wpP0chF3iATnjZRFCec8_S8x8tBD5mXi/view"
+                    target="_blank"
+                    >Academic Transcript</a
+                  >
+                </p>
+                <hr />
+              </div>
+              <div class="w3-container">
+                <h5 class="w3-opacity"><b>BRAC University</b></h5>
+                <h6 class="w3-text-teal">
+                  <i class="fa fa-calendar fa-fw w3-margin-right"></i>June 2022 - May 2024
+                </h6>
+                <p>
+                  Undergraduate minor in Computer Science (CS) <br />
+                  BRAC University | Minor: Computer Science | CS Minor CGPA: 3.97
+                </p>
+                <hr />
+              </div>
+              <div class="w3-container">
+                <h5 class="w3-opacity"><b>South Point School and College</b></h5>
+                <h6 class="w3-text-teal">
+                  <i class="fa fa-calendar fa-fw w3-margin-right"></i>July 2017 - June 2019
+                </h6>
+                <p>
+                  British Council Cambridge (GCE A Levels)<br />
+                  Results: 4 A*/As in Accounting, Pure Mathematics, Physics & Chemistry <br />
+                  Awards: Daily Star Award and 5th in the English Medium Math Olympiad 2017
+                  organized by UIU
+                </p>
+                <br />
+              </div>
+              <div class="w3-container">
+                <h5 class="w3-opacity"><b>Shaheen English Medium School</b></h5>
+                <h6 class="w3-text-teal">
+                  <i class="fa fa-calendar fa-fw w3-margin-right"></i>July 2014 - June 2017
+                </h6>
+                <p>
+                  British Council Cambridge (GCE O Levels)<br />
+                  Results: 7A*/As, 1 B | Commerce & Science Background | Awards: Daily Star Award<br />
+                  Awards: Daily Star Award and Chief of Air Staff Award
+                </p>
+                <br />
+              </div>
+            </div>
+
+            <!-- End Right Column -->
+          </div>
+
+          <!-- End Grid -->
+        </div>
+
+        <!-- End Page Container -->
+      </div>
+
+      <footer class="w3-container w3-teal w3-center w3-margin-top">
+        <p>Find me on social media.</p>
+        <a href="https://www.facebook.com/TripleM.Meshkat" target="_blank"
+          ><i class="fa fa-facebook-official w3-hover-opacity"></i
+        ></a>
+        <a href="https://www.instagram.com/triplem_meshkat100/" target="_blank"
+          ><i class="fa fa-instagram w3-hover-opacity"></i
+        ></a>
+        <a href="https://www.linkedin.com/in/mashook-mohammad-meshkat" target="_blank"
+          ><i class="fa fa-linkedin w3-hover-opacity"></i
+        ></a>
+        <p>
+          Powered by
+          <a
+            href="https://drive.google.com/file/d/1wpP0chF3iATnjZRFCec8_S8x8tBD5mXi/view"
+            target="_blank"
+            >ME</a
+          >
+        </p>
+      </footer>
+    </body>
+  </main>
+
+  <RouterView />
+</template>
+
+<style scoped>
+html,
+body,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-family: 'Roboto', sans-serif;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
